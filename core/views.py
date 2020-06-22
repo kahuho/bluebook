9da927fdb5ab315284df413c8745c2e468ebc341
kahuho <kahuhobrian7@gmail.com>
2020-06-22 15:18:14 +0300

    minor changes

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView, DetailView
 from .models import Item, Order, OrderItem
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.contrib import messages
 
 
 def home(request):
@@ -50,14 +51,19 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug). exists():
             order_item.quantity += 1
             order_item.save()
+            messages.info(request, "Item quanity was updated")
+
         else:
             order.items.add(order_item)
+            messages.info(request, "Item succcesfully added to your cart")
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
             user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
     return redirect("core:product", slug=slug)
+
+
 # removing items from the cart
 
 
@@ -67,11 +73,17 @@ def remove_from_cart(request, slug):
     if order_qs.exists():
         order = order_qs[0]
     # confirm whether item alreday exists in order
-        if order.items.filter(item__slug=item.slug). exists():
+        if order.items.filter(item__slug=item.slug).exists():
             order_item = OrderItem.objects.filter(item=item,
                                                   user=request.user, ordered=False)[0]
             order.items.remove(order_item)
+            messages.warning(request, "Item removed from your cart")
+
+        else:
+            # add message order doesnt exist
+            messages.info(request, "Item does not exists in your cart")
+            return redirect("core:product", slug=slug)
     else:
-            # add message order doesnt exisst
-        return redirect("core:product", slug=slug)
+        messages.info(request, "You do not have an active order yet")
+        return redirect("core:product")
     return redirect("core:product", slug=slug)
--- demo/settings.py
+++ demo/settings.py
@@ -74,8 +74,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "brykahn",
+        "HOST": "localhost",
+
     }
 }
 
